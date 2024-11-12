30455442f76c5f2a4576e66611a4784420df016b
MayElbaz18 <may.elbaz12@gmail.com>
2024-11-12 10:55:52 +0200

    app.py Fix

--- exercise_folder/DEV_F/app.py
+++ exercise_folder/DEV_F/app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, jsonify, request
 import re
 import json
-import os
 
 
-app = Flask('__name__')
+app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
